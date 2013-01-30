d568da5098c64cc483e22b2daadf36ac8ce43fee
Matt Rubin <mattrubin@me.com>
2013-01-30 01:15:01 -0800

    Initial Commit

---
+++ Penumbra/PENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PENAppDelegate.h
+//  Penumbra
+//
+//  Created by Me on 1/30/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Penumbra/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Penumbra
+//
+//  Created by Me on 1/30/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PENAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PENAppDelegate class]));
+    }
+}
